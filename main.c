cb28a205aacb8f918e410d72ed8d5d47bc806893
Sylvester Ink <sylvesterink@gmail.com>
2012-08-30 13:58:53 -0700

    Structure of main, also created second test program.  Created initial, unusable makefile

--- main.c
+++ main.c
@@ -1,29 +1,47 @@
-#include <avr/io.h> 
+/**
+ * project: Lab 1, parts 1 & 2
+ * @file main.c
+ * @brief
+ * @author Cameron Bently, Brandon Kasa
+ * @date 2012-08-30
+ * build:
+ */
+#include <avr/io.h>
 
-void wait_ms(unsigned int milliseconds);
+#define CYCLES_PER_MS 100
+
+void wait_ms(unsigned int time);
 
 /**
- * @brief 
+ * @brief
  * @param argc
  * @param argv[]
- * @return 
+ * @return
  */
 int main(int argc, char const *argv[])
 {
-    wait_ms(1000);
+    /*DDRE = <port direction?>;*/
+    /*PORTE = <port output?>;*/
+
+    while (1) {
+        wait_ms(250);
+        // 	count on port E somehow?
+
+    }
+
     return 0;
 }
 
 /**
- * @brief 
+ * @brief
  * @param milliseconds
  */
-void wait_ms(unsigned int milliseconds)
+void wait_ms(unsigned int time)
 {
-    int temp=0;
-    for (unsigned int i = 0; i < milliseconds; ++i)
-    {
-        temp++;
+    unsigned int cycles = time * CYCLES_PER_MS;
+
+    while (cycles > 0) {
+        cycles--;
     }
 }
 
