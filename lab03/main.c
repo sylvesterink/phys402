26fb7f689702d3b6318b7809b41f4c71cfe2d90b
Cameron Bentley <bentl011@csusm.edu>
2012-09-13 15:15:28 -0700

    added framework from lab document

--- lab03/main.c
+++ lab03/main.c
@@ -1,73 +1,30 @@
 /**
  * project: Lab 2, part 1
  * @file part1.c
- * @brief Set a timer to implement a binary counter using the LEDs at the
- *        specified time rate
+ * @brief 
+ *        
  * @author Cameron Bentley, Brandon Kasa
  * @date 2012-09-13
  * build: 1.0
  */
-#include <avr/io.h>
+
 #include <avr/interrupt.h>
+#include <avr/io.h>
+#include <AVRXlib/AVRX_Clocks.h>
 
-/*With clock divider at 0x03, a full timer interval is 1 second*/
-/*The clock delay is set here*/
-#define MILLISECONDS 25
-#define CLOCK_OFFSET (1000 - MILLISECONDS) * 65.535
 
-/**
- * @brief Timer interrupt for when the timeout occurs
- * @param TIMER1_OVF_vect Type of interrupt
- */
-ISR(TIMER1_OVF_vect)
+void main(void)
 {
-    /*Increment the binary counter*/
-    PORTE += 0x01;
-
-    //cli();//turns off interrupts
-
-    /*Start timer at a later point*/
-    /*To get exact timer granularity, uncomment below*/
-    /* At 0x03 clock scale, this will be 16us */
-    /*TCNT1 = 65535; */
-
-    /*This will allow us to set our own interval*/
-    TCNT1 = CLOCK_OFFSET;
-}
-
-/**
- * @brief Main program loop.  Repeatedly counts from 0-15 using the onboard
- *        LEDs as indicators.
- * @param argc Argument count
- * @param argv[] Argument list
- * @return Error code
- */
-int main(int argc, char const *argv[])
-{
-    /*Set the high bits of port E (LED bits) as output bits*/
-    DDRE = 0xF0;
-    /*Set all the LEDs off initially*/
-    PORTE = 0x00;
-
-    /*Disable other timer modes*/
-    TCCR1A = 0x00;
-    TCCR1C = 0x00;
-
-    /*Clock divider*/
-    /*Divide clock*/
-    TCCR1B = 0x03;
-
-    /*Enable timer overflow interrupt*/
-    TIMSK = 0x04;
-
-    /*Clear timer overflow flag register*/
-    TIFR = 0x00;
-
-    sei();
-
-    /* Run continuously */
-    while (1) {
-    }
-
-    return 0;
+	unsigned long sClk, pClk;
+	
+	cli();
+	
+	SetSystemClock(CLK_SCLKSEL_RC32M_gc, CLK_PSADIV_1_gc, CLK_PSBCDIV_1_1_gc);
+	GetSystemClocks(&sClk, &pClk);
+	
+	PMIC.CTRL = PMIC_HILVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_LOLVLEN_bm;
+	
+	sei();
+	
+	// other code
 }
