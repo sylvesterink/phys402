dd736612162ff7c692eae7b7199f1eeeda56627b
Sylvester Ink <sylvesterink@gmail.com>
2012-09-27 13:19:56 -0700

    Set up Lab 4

---
+++ lab04/part1.c
@@ -0,0 +1,35 @@
+/**
+ * project: Lab 4, part 1
+ * @file part1.c
+ * @brief 
+ * @author Cameron Bentley, Brandon Kasa
+ * @date 2012-09-27
+ * build: 1.0
+ */
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+/*#include <AVRXlib/AVRX_Clocks.h>*/
+/*#include <math.h>*/
+
+/*Define our own boolean values for readability*/
+#define FALSE 0
+#define TRUE 1
+
+ISR()
+{
+}
+
+/**
+ * @brief 
+ * @param argc Argument count
+ * @param argv[] Argument list
+ * @return Error code
+ */
+int main(int argc, char const *argv[])
+{
+/************ PROGRAM LOOP *************/	
+	while(1)
+	{
+	}
+}
