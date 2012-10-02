f93f7e3461ae58c5e5de8dfd720ca71c14d622d1
Cameron Bentley <bentl011@csusm.edu>
2012-10-02 16:11:07 -0700

    Completed part 2 and got the dimmer to work on the button.

--- lab04/part2.c
+++ lab04/part2.c
@@ -9,15 +9,33 @@
 
 #include <avr/interrupt.h>
 #include <avr/io.h>
-/*#include <AVRXlib/AVRX_Clocks.h>*/
+#include <AVRXlib/AVRX_Clocks.h>
+
+#define CYCLE_LENGTH 5000
+#define CCA_LENGTH 4800
+
+static volatile short TIMER_STEP = 1;
+
+ISR(PORTJ_INT0_vect)
+{
+	TIMER_STEP *= -1;
+	TCC0_CCA += TIMER_STEP;
+	PORTH_OUT = 0x00;
+}
+
+ISR(TCC0_CCA_vect)
+{
+	PORTH_OUT = 0x00;	
+}
 
 
 ISR(TCC0_OVF_vect)
 {
-	!!! Don't forget about update rate changing
-	!!! port j will be used for the pushbuttons
-	TCC0_CCA--;
-	if()
+	if ( (TCC0_CCA > 0) && (TCC0_CCA < CCA_LENGTH) )
+	{
+		TCC0_CCA += TIMER_STEP;
+	}
+	PORTH_OUT = 0x01;
 }
 
 /**
@@ -28,7 +46,14 @@ ISR(TCC0_OVF_vect)
  */
 int main(int argc, char const *argv[])
 {
+	unsigned long sClk, pClk;
+	cli();
 	
+/************ SET UP SYSTEM CLOCK *************/	
+	SetSystemClock(CLK_SCLKSEL_RC32M_gc, CLK_PSADIV_1_gc, CLK_PSBCDIV_1_1_gc);
+	GetSystemClocks(&sClk, &pClk);
+	
+	PMIC.CTRL = PMIC_HILVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_LOLVLEN_bm;
 /************ SET UP TIMERS *************/
   
 	TCC0_CTRLA = TC_CLKSEL_DIV8_gc; /*Set clock divider*/
@@ -36,15 +61,32 @@ int main(int argc, char const *argv[])
 	TCC0_CTRLC = 0x00; /*Turn waveform generation output compare off*/
 	TCC0_CTRLD = 0x00;  /*Turn off event action*/
 	TCC0_CTRLE = 0x00; /*Sets timer to 16bit mode*/
-	TCC0_INTCTRLA = 0x02; /*Sets the interrupt to overflow off*/
-	TCC0_INTCTRLB = 0x00; /*Sets the Compare or Capture interrupt off*/
-	TCC0_PER = 2000;
-	TCC0_CCA = 160;
+	TCC0_INTCTRLA = 0x02; /*Sets the interrupt to overflow to med priority*/
+	TCC0_INTCTRLB = 0x02; /*Sets the Compare or Capture interrupt to med priority*/
+	TCC0_PER = CYCLE_LENGTH; /* Cycle length */
+	TCC0_CCA = CCA_LENGTH; /* Capture and Compare point during cycle */
 	
-	PORTC_DIRSET = 0xFF;
+/***************SET UP BUTTONS*******************/
+	
+	PORTJ_DIR = 0x00;
+	PORTJ_INTCTRL = 0x01; /*sets interrupt 0 to med priority*/
+	PORTJ_INT0MASK = 0xFF; /*sets all button pins to trigger interrupt*/
+	PORTCFG_MPCMASK = 0xff;
+	PORTJ_PIN0CTRL = 0x01;
+
+	
+	/*Set LED1 to output*/
+	PORTH_DIR = 0xFF;
+	PORTH_OUT = 0x01;
+	
+	sei();
+	
+	//PORTC_DIRSET = 0xFF;
+	
 
 /************ PROGRAM LOOP *************/	
 	while(1)
 	{
+		//PORTH_OUT = 0x00;
 	}
 }
